9f91f8af4fb4e53866e30f256a5b1a7ef2311e50
mansi <mansi.chourasia@cgi.com>
2019-07-23 12:49:00 +0530

    updated comments for multipleOccurenceOfCharector file

--- src/main/java/com/stackroute/pe4/MultipleOccurenceOfCharector.java
+++ src/main/java/com/stackroute/pe4/MultipleOccurenceOfCharector.java
@@ -6,16 +6,20 @@ import java.util.regex.Pattern;
 
 public class MultipleOccurenceOfCharector {
     public String findOccuence(String string,String inputData) {
+        /* if given inputString is null */
         if(inputData==null)
         {
             return "please provide valid string";
         }
         else
         {
+            /* get the instance of given pattern which you wanted to find */
     Pattern pattern= Pattern.compile(string);
+            /* match the instance of given input data string */
         Matcher matcher=pattern.matcher(inputData);
         String result="";
         while(matcher.find())
+        // store matched pattern index start value and end value
         {
             result=result+matcher.start()+"-"+matcher.end()+" ";
         }
